015f338f1daa0190c19cd4ad9f576ee073e0efe0
StevenPTurner <spt10pd@msn.com>
2016-11-18 12:13:27 +0000

    Started to create schemea for database

---
+++ app/src/main/java/com/medtracker/database/MedTrackerContract.java
@@ -0,0 +1,8 @@
+package com.medtracker.database;
+
+/**
+ * Created by home on 18/11/2016.
+ */
+
+public final class MedTrackerContract {
+}
---
+++ app/src/main/java/com/medtracker/database/MedtrackerDbHelper.java
@@ -0,0 +1,35 @@
+package com.medtracker.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by home on 18/11/2016.
+ */
+
+public class MedtrackerDbHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "MedTracker.db";
+
+
+    public MedtrackerDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+        //db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        //db.execSQL(SQL_DELETE_ENTRIES);
+        //onCreate(db);
+    }
+
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+}
