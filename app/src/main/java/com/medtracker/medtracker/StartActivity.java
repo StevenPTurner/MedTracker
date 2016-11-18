0b7d571a952752557d76e125498ef6b1bc7dd1ef
StevenPTurner <spt10pd@msn.com>
2016-11-18 11:07:52 +0000

    more temp ui including register page

--- app/src/main/java/com/medtracker/medtracker/StartActivity.java
+++ app/src/main/java/com/medtracker/medtracker/StartActivity.java
@@ -1,7 +1,9 @@
 package com.medtracker.medtracker;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartActivity extends Activity {
 
@@ -9,5 +11,10 @@ public class StartActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+
+    public void createAccountActivity(View view){
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
     }
 }
