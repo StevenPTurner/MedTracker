cec7e9882fc4f62fedbe5a85f2b56894af232c1a
StevenPTurner <spt10pd@msn.com>
2016-11-18 12:58:50 +0000

    Created structure for database

--- app/src/main/java/com/medtracker/database/MedTrackerContract.java
+++ app/src/main/java/com/medtracker/database/MedTrackerContract.java
@@ -1,8 +1,36 @@
 package com.medtracker.database;
 
+import android.provider.BaseColumns;
+
 /**
  * Created by home on 18/11/2016.
  */
 
 public final class MedTrackerContract {
+
+    private MedTrackerContract() {}
+
+    public static class User implements BaseColumns {
+        public static final String TABLE_NAME = "user";
+        public static final String COLUMN_NAME_EMAIL = "email";
+        public static final String COLUMN_NAME_FIRST_NAME = "first_name";
+        public static final String COLUMN_NAME_LAST_NAME = "last_name";
+        public static final String COLUMN_NAME_PIN = "pin";
+        public static final String COLUMN_NAME_GOOGLE_ACCOUNT = "google_account";
+        public static final String SQL_CREATE_TABLE_USER =
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                COLUMN_NAME_EMAIL + " TEXT PRIMARY KEY," +
+                COLUMN_NAME_FIRST_NAME + " TEXT," +
+                COLUMN_NAME_LAST_NAME + " TEXT," +
+                COLUMN_NAME_PIN + " INT" +
+                COLUMN_NAME_GOOGLE_ACCOUNT + " BOOLEAN )";
+    }
+
+    public static class Medication implements BaseColumns {}
+    public static class Prescription implements BaseColumns {}
+    public static class Record implements BaseColumns {}
+    public static class Alarm implements BaseColumns {}
+    public static class AlarmManager implements BaseColumns {}
+    public static class Glossary implements BaseColumns {}
+    public static class Pharmacies implements BaseColumns {}
 }
--- app/src/main/java/com/medtracker/database/MedtrackerDbHelper.java
+++ app/src/main/java/com/medtracker/database/MedtrackerDbHelper.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import static com.medtracker.database.MedTrackerContract.User.SQL_CREATE_TABLE_USER;
+
 /**
  * Created by home on 18/11/2016.
  */
@@ -19,14 +21,14 @@ public class MedtrackerDbHelper extends SQLiteOpenHelper {
     }
 
     public void onCreate(SQLiteDatabase db) {
-        //db.execSQL(SQL_CREATE_ENTRIES);
+        db.execSQL(SQL_CREATE_TABLE_USER);
     }
 
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // This database is only a cache for online data, so its upgrade policy is
         // to simply to discard the data and start over
         //db.execSQL(SQL_DELETE_ENTRIES);
-        //onCreate(db);
+        onCreate(db);
     }
 
     public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
