e917bfe07fe986126ab830f5f487573c5c1f53ef
StevenPTurner <spt10pd@msn.com>
2016-11-19 19:55:49 +0000

    database works 100%

--- app/src/main/java/com/medtracker/medtracker/CreateAccountActivity.java
+++ app/src/main/java/com/medtracker/medtracker/CreateAccountActivity.java
@@ -1,7 +1,13 @@
 package com.medtracker.medtracker;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.medtracker.database.MedtrackerDbHelper;
 
 public class CreateAccountActivity extends Activity {
 
@@ -9,5 +15,23 @@ public class CreateAccountActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+    }
+
+    public void createAccount(View view) {
+        MedtrackerDbHelper db = new MedtrackerDbHelper(this);
+        //Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editTextFirstName = (EditText) findViewById(R.id.edittext_firstname);
+        EditText editTextLastName = (EditText) findViewById(R.id.edittext_lastname);
+        EditText editTextEmail = (EditText) findViewById(R.id.edittext_email);
+
+        String email = editTextEmail.getText().toString();
+        String firstName = editTextFirstName.getText().toString();
+        String lastName = editTextLastName.getText().toString();
+
+        db.createUser(email, firstName, lastName, 1111, false);
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        //startActivity(intent);
+        TextView text = (TextView) findViewById(R.id.textview_register_account);
+        text.setText("yay!");
     }
 }
