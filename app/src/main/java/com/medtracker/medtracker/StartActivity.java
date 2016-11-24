ecfbd39fb8bb0705b43b78f991e0d99181e63ca8
StevenPTurner <spt10pd@msn.com>
2016-11-24 19:54:00 +0000

    refactoring to start adding firebase support

--- app/src/main/java/com/medtracker/medtracker/StartActivity.java
+++ app/src/main/java/com/medtracker/medtracker/StartActivity.java
@@ -31,6 +31,7 @@ public class StartActivity extends FragmentActivity implements
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
@@ -53,27 +54,36 @@ public class StartActivity extends FragmentActivity implements
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
-    private void handleSignInResult(GoogleSignInResult result) {
-        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
-        TextView text = (TextView) findViewById(R.id.textview_title);
-
-        if (result.isSuccess()) {
-            // Signed in successfully, show authenticated UI.
-            GoogleSignInAccount acct = result.getSignInAccount();
-            text.setText("success");
-        } else {
-            text.setText("Success");
-        }
-    }
+//    private void handleSignInResult(GoogleSignInResult result) {
+//        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+//        TextView text = (TextView) findViewById(R.id.textview_title);
+//
+//        if (result.isSuccess()) {
+//            // Signed in successfully, show authenticated UI.
+//            GoogleSignInAccount acct = result.getSignInAccount();
+//            String personName = acct.getDisplayName();
+//            text.setText(personName);
+//        } else {
+//            text.setText("Success");
+//        }
+//    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
+        TextView text = (TextView) findViewById(R.id.textview_title);
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleSignInResult(result);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                //firebaseAuthWithGoogle(account);
+                String personName = account.getDisplayName();
+                text.setText(personName);
+            } else {
+                text.setText("Success");
+            }
         }
     }
 
