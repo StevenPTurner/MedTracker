02a96c5ab1966d8480fcf77a399d924b7ab2f703
StevenPTurner <spt10pd@msn.com>
2017-01-23 20:04:20 +0000

    WIP switching machines

---
+++ app/src/main/java/com/medtracker/Activities/HomeActivity.java
@@ -0,0 +1,80 @@
+package com.medtracker.Activities;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.medtracker.medtracker.R;
+
+public class HomeActivity extends Activity {
+    private String[] mMenuTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+        mMenuTitles = getResources().getStringArray(R.array.menu_titles_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mMenuTitles));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    /** Swaps fragments in the main content view */
+    private void selectItem(int position) {
+        // Create a new fragment and specify the planet to show based on position
+        Fragment fragment;
+        FragmentManager fragmentManager = getFragmentManager();
+        Bundle args = new Bundle();
+
+        switch(position) {
+            case 0:
+                //fragment = new Medications();
+                //args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
+                break;
+            case 1:
+                //fragment = new MyFragment2();
+                break;
+        }
+
+
+        fragment.setArguments(args);
+        // Insert the fragment by replacing any existing fragment
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+
+        // Highlight the selected item, update the title, and close the drawer
+        mDrawerList.setItemChecked(position, true);
+        setTitle(mMenuTitles[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getActionBar().setTitle(mTitle);
+    }
+
+}
