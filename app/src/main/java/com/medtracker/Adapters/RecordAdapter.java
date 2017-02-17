6c570b1e43f1e29685601a7c52b8b441bfb9612d
StevenPTurner <spt10pd@msn.com>
2017-02-17 18:21:22 +0000

    formatted records table

--- app/src/main/java/com/medtracker/Adapters/RecordAdapter.java
+++ app/src/main/java/com/medtracker/Adapters/RecordAdapter.java
@@ -1,10 +1,12 @@
 package com.medtracker.Adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.medtracker.Models.Record;
@@ -20,19 +22,28 @@ import java.util.Calendar;
  */
 
 public class RecordAdapter extends ArrayAdapter<Record> {
+    private int count;
 
     public RecordAdapter(Context context, ArrayList<Record> records) {
         super(context, 0, records);
+        count = 0;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+
         // Get the data item for this position
         Record record = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.adapter_item_record, parent, false);
         }
+
+        RelativeLayout layout = (RelativeLayout) convertView.findViewById(R.id.table_row);
+
+        if(position % 2 == 0)
+            layout.setBackgroundColor(Color.parseColor("#BBDEFB"));
+
         // Lookup view for data population
         TextView recordName = (TextView) convertView.findViewById(R.id.record_name);
         TextView recordDosage = (TextView) convertView.findViewById(R.id.record_dosage);
