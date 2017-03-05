861fc19ba0badf9ee7ea95aa3f94a6187db9103c
StevenPTurner <spt10pd@msn.com>
2017-03-05 13:02:27 +0000

    Refactored alarm manager Model

--- app/src/main/java/com/medtracker/Models/Alarm.java
+++ app/src/main/java/com/medtracker/Models/Alarm.java
@@ -16,7 +16,8 @@ public class Alarm {
 
     public Alarm() {}
 
-    public Alarm(int id, int minute, int hour, int day, int month, int year, String medication_key, int RCID) {
+    public Alarm(int id, int minute, int hour, int day, int month, int year, String medication_key,
+                 int RCID) {
         this.id = id;
         this.minute = minute;
         this.hour = hour;
@@ -30,24 +31,31 @@ public class Alarm {
     public int getMinute() {
         return minute;
     }
+
     public int getHour() {
         return hour;
     }
+
     public int getDay() {
         return day;
     }
+
     public int getMonth() {
         return month;
     }
+
     public int getYear() {
         return year;
     }
+
     public int getId() {
         return id;
     }
+
     public String getMedication_key() {
         return medication_key;
     }
+
     public int getRCID() {
         return RCID;
     }
@@ -55,24 +63,31 @@ public class Alarm {
     public void setMinute(int minute) {
         this.minute = minute;
     }
+
     public void setHour(int hour) {
         this.hour = hour;
     }
+
     public void setDay(int day) {
         this.day = day;
     }
+
     public void setMonth(int month) {
         this.month = month;
     }
+
     public void setYear(int year) {
         this.year = year;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public void setMedication_key(String medication_key) {
         this.medication_key = medication_key;
     }
+
     public void setRCID(int RCID) {
         this.RCID = RCID;
     }
--- app/src/main/java/com/medtracker/Models/AlarmManager.java
+++ app/src/main/java/com/medtracker/Models/AlarmManager.java
@@ -1,7 +1,7 @@
 package com.medtracker.Models;
 
 /**
- * Created by spt10 on 08/02/2017.
+ * Model for alarm manager object
  */
 
 public class AlarmManager {
@@ -11,11 +11,10 @@ public class AlarmManager {
     private int max_count;
     private int current_count;
 
-    public AlarmManager() {
+    public AlarmManager() {}
 
-    }
-
-    public AlarmManager(String medication_key, boolean has_alarm, String current_alarm, int max_count, int current_count) {
+    public AlarmManager(String medication_key, boolean has_alarm, String current_alarm,
+                        int max_count, int current_count) {
         this.medication_key = medication_key;
         this.has_alarm = has_alarm;
         this.current_alarm = current_alarm;
@@ -27,42 +26,39 @@ public class AlarmManager {
         return current_count;
     }
 
-    public void setCurrent_count(int current_count) {
-        this.current_count = current_count;
-    }
-
     public String getMedication_key() {
         return medication_key;
-    }
-
-    public void setMedication_key(String medication_key) {
-        this.medication_key = medication_key;
     }
 
     public boolean isHas_alarm() {
         return has_alarm;
     }
 
-    public void setHas_alarm(boolean has_alarm) {
-        this.has_alarm = has_alarm;
-    }
-
     public String getCurrent_alarm() {
         return current_alarm;
-    }
-
-    public void setCurrent_alarm(String current_alarm) {
-        this.current_alarm = current_alarm;
     }
 
     public int getMax_count() {
         return max_count;
     }
 
+    public void setCurrent_count(int current_count) {
+        this.current_count = current_count;
+    }
+
+    public void setMedication_key(String medication_key) {
+        this.medication_key = medication_key;
+    }
+
+    public void setHas_alarm(boolean has_alarm) {
+        this.has_alarm = has_alarm;
+    }
+
+    public void setCurrent_alarm(String current_alarm) {
+        this.current_alarm = current_alarm;
+    }
+
     public void setMax_count(int max_count) {
         this.max_count = max_count;
     }
-
-
-
 }
