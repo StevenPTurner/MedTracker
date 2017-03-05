4363e92d80a61bfd8df8c54e2b9681f63bc04f94
StevenPTurner <spt10pd@msn.com>
2017-03-05 12:57:06 +0000

    Refactored alarm Model

--- app/src/main/java/com/medtracker/Models/Alarm.java
+++ app/src/main/java/com/medtracker/Models/Alarm.java
@@ -1,7 +1,7 @@
 package com.medtracker.Models;
 
 /**
- * Created by spt10 on 08/02/2017.
+ * Model for alarm objects
  */
 
 public class Alarm {
@@ -14,9 +14,7 @@ public class Alarm {
     private String medication_key;
     private int RCID;
 
-    public Alarm() {
-
-    }
+    public Alarm() {}
 
     public Alarm(int id, int minute, int hour, int day, int month, int year, String medication_key, int RCID) {
         this.id = id;
@@ -32,63 +30,49 @@ public class Alarm {
     public int getMinute() {
         return minute;
     }
-
-    public void setMinute(int minute) {
-        this.minute = minute;
-    }
-
     public int getHour() {
         return hour;
     }
-
-    public void setHour(int hour) {
-        this.hour = hour;
-    }
-
     public int getDay() {
         return day;
     }
-
-    public void setDay(int day) {
-        this.day = day;
-    }
-
     public int getMonth() {
         return month;
     }
-
-    public void setMonth(int month) {
-        this.month = month;
-    }
-
     public int getYear() {
         return year;
     }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
     public int getId() {
         return id;
     }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getMedication_key() {
         return medication_key;
     }
-
-    public void setMedication_key(String medication_key) {
-        this.medication_key = medication_key;
-    }
-
     public int getRCID() {
         return RCID;
     }
 
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+    public void setDay(int day) {
+        this.day = day;
+    }
+    public void setMonth(int month) {
+        this.month = month;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setMedication_key(String medication_key) {
+        this.medication_key = medication_key;
+    }
     public void setRCID(int RCID) {
         this.RCID = RCID;
     }
