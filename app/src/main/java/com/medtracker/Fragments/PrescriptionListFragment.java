d89304b88a47caacf636dd1f63755f91be23be6a
StevenPTurner <spt10pd@msn.com>
2017-03-05 01:38:29 +0000

    cleaning up prescription class & removing place picker

--- app/src/main/java/com/medtracker/Fragments/PrescriptionListFragment.java
+++ app/src/main/java/com/medtracker/Fragments/PrescriptionListFragment.java
@@ -24,63 +24,25 @@ import static android.app.Activity.RESULT_OK;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PrescriptionListFragment extends Fragment implements
-        GoogleApiClient.OnConnectionFailedListener {
-
-    private GoogleApiClient mGoogleApiClient;
-    int PLACE_PICKER_REQUEST = 1;
-    PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+public class PrescriptionListFragment extends Fragment {
 
     public PrescriptionListFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the adapter_item_alarm_manager for this fragment
         return inflater.inflate(R.layout.fragment_prescription_list, container, false);
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        mGoogleApiClient = new GoogleApiClient
-                .Builder(getActivity())
-                .addApi(Places.GEO_DATA_API)
-                .addApi(Places.PLACE_DETECTION_API)
-                .enableAutoManage((FragmentActivity) getActivity(), this)
-                .build();
-
     }
 
     @Override
     public void onStart() {
         super.onStart();
-
-        try {
-            startActivityForResult(builder.build(getActivity()), PLACE_PICKER_REQUEST);
-        } catch(Exception e) {
-
-        }
     }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult result) {
-        // Refer to the reference doc for ConnectionResult to see what error codes might
-        // be returned in onConnectionFailed.
-    }
-
-     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == PLACE_PICKER_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                Place place = PlacePicker.getPlace(data, getActivity().getApplicationContext());
-                String toastMsg = String.format("Place: %s", place.getName());
-                Toast.makeText(getActivity().getApplicationContext(), toastMsg, Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-
 }
