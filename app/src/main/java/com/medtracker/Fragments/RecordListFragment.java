a5e2c4a416b7eb67b90b7431e154d8d5fff6865c
StevenPTurner <spt10pd@msn.com>
2017-03-14 08:43:57 +0000

    finished formatting e-mail button

--- app/src/main/java/com/medtracker/Fragments/RecordListFragment.java
+++ app/src/main/java/com/medtracker/Fragments/RecordListFragment.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
